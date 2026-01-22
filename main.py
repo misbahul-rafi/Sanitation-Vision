08c527e171e15bc86487dd8c5208749956f9f372
misbahul-rafi <misbahulrafi24@gmail.com>
2026-01-22 21:40:32 +0700

    ADD: formatter timestamp to logger

--- main.py
+++ main.py
@@ -47,14 +47,21 @@ class SanitationApp:
         logger.setLevel(logging.INFO)
 
         if not logger.handlers:
+            formatter = logging.Formatter(
+                "%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+                datefmt="%Y-%m-%d %H:%M:%S"
+            )
+
             stream_handler = logging.StreamHandler()
+            stream_handler.setFormatter(formatter)
+
             file_handler = logging.FileHandler("logs/sanitation.log", encoding="utf-8")
+            file_handler.setFormatter(formatter)
 
             logger.addHandler(stream_handler)
             logger.addHandler(file_handler)
-            
-        logger.info('handler ready')
 
+        logger.info("handler ready")
         return logger
 
 
