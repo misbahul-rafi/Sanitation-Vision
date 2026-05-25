9b2484e25ca783d7315ccdb34f3d7e436e1ffd23
misbahul-rafi <misbahulrafi24@gmail.com>
2026-05-25 20:33:00 +0700

    UPDATE: buffer_status send to dashboard

--- modules/table.py
+++ modules/table.py
@@ -63,6 +63,7 @@ class Table:
         return {
             "id": self._id,
             "status": self._status,
+            "buffer": self._status_buffer
         }
     def insert_items(self, item_name):
         try:
